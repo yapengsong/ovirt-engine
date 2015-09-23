46e9744cfb08fa6a4461b7c6699de27163da2a8b
root <root@localhost.localdomain>
2015-09-23 04:06:26 -0400

    Add macspoof custom device properties
    needed for neutron intergration
    
    Signed-off-by: root <root@localhost.localdomain>

---
+++ packaging/dbscripts/upgrade/03_05_1300_update_custom_device_properties.sql
@@ -0,0 +1,1 @@
+select fn_db_update_config_value('CustomDeviceProperties','{type=interface;prop={SecurityGroups=^(?:(?:[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}, *)*[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}|);ifacemacspoof=^(true|false)$}}','3.5');
