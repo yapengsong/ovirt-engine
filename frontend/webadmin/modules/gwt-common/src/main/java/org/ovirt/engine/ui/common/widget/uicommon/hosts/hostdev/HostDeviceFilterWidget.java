867be7b66ec785ecb85ba06be1c1df3ac7eb6b7d
dengxinchao <475356551@qq.com>
2016-12-09 15:41:27 +0800

    修复Feature #9020 【主机】【主机设备】添加过滤说明

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/hosts/hostdev/HostDeviceFilterWidget.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/hosts/hostdev/HostDeviceFilterWidget.java
@@ -6,9 +6,12 @@ import org.ovirt.engine.ui.common.CommonApplicationConstants;
 import org.ovirt.engine.ui.common.gin.AssetProvider;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
+import org.ovirt.engine.ui.common.widget.WidgetWithInfo;
+import org.ovirt.engine.ui.common.widget.tooltip.TooltipConfig.Width;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -45,6 +48,9 @@ public class HostDeviceFilterWidget extends Composite {
     @UiField(provided = true)
     Button editButton;
 
+    @UiField(provided = true)
+    WidgetWithInfo filterline;
+
     private final static CommonApplicationConstants constants = AssetProvider.getConstants();
 
     @Inject
@@ -59,9 +65,37 @@ public class HostDeviceFilterWidget extends Composite {
         enableFilter = new CheckBox();
         enableFilter.setText(constants.enableFilterOfHostDevice());
         enableFilter.setValue(true);
+
+        filterline = new WidgetWithInfo(enableFilter);
+        filterline.setExplanation(SafeHtmlUtils.fromTrustedString(createSpiceInvocationInfo()));
+        filterline.addInfoIconStyle("cpv_infoIcon_pfly_fix"); //$NON-NLS-1$
+        filterline.setInfoIconTooltipMaxWidth(Width.W420);
+
         editButton = new Button();
         editButton.setText(constants.editFilterOfHostDevice());
         editButton.setVisible(false);
+    }
+
+    private String createSpiceInvocationInfo() {
+        return new KeyValueHtmlRowMaker(constants.filter(), constants.filterexplain()).toString();
+    }
+    private class KeyValueHtmlRowMaker {
+
+        private String html;
+
+        private KeyValueHtmlRowMaker(String key, String val) {
+            html = "<b>" + key + "</b>: " + val; //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        //可以在以后添加新的解释
+        public KeyValueHtmlRowMaker append(String key, String val) {
+            html += "<br/>" + new KeyValueHtmlRowMaker(key, val).toString(); //$NON-NLS-1$
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return html;
+        }
     }
 
     public void addValueChangeHandler(ValueChangeHandler<Boolean> handler) {
