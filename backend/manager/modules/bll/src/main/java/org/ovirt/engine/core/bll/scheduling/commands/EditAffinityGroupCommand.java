045d925fe0fe6eae5819832456ac7e5c8b0de3e4
wxt <Sline798@gmail.com>
2017-03-10 18:11:00 +0800

    webadmin: About affinity group.
    Fix Bug #9127. See Bug 7647.
    
    Signed-off-by: wxt <Sline798@gmail.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/commands/EditAffinityGroupCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/commands/EditAffinityGroupCommand.java
@@ -1,8 +1,12 @@
 package org.ovirt.engine.core.bll.scheduling.commands;
 
+import java.util.List;
+import org.apache.commons.collections.ListUtils;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.errors.EngineMessage;
+import org.ovirt.engine.core.common.scheduling.AffinityGroup;
 import org.ovirt.engine.core.common.scheduling.parameters.AffinityGroupCRUDParameters;
+import org.ovirt.engine.core.compat.Guid;
 
 
 public class EditAffinityGroupCommand extends AffinityGroupCRUDCommand {
@@ -22,6 +26,16 @@ public class EditAffinityGroupCommand extends AffinityGroupCRUDCommand {
         if (!getAffinityGroup().getName().equals(getParameters().getAffinityGroup().getName()) &&
                 getAffinityGroupDao().getByName(getParameters().getAffinityGroup().getName()) != null) {
             return failCanDoAction(EngineMessage.ACTION_TYPE_FAILED_AFFINITY_GROUP_NAME_EXISTS);
+        }
+        List<Guid> vmIdList = getAffinityGroup().getEntityIds();
+        boolean isEnforcing = getAffinityGroup().isEnforcing();
+        boolean isPositive = getAffinityGroup().isPositive();
+        for(AffinityGroup ag : getAffinityGroupDao().getAllAffinityGroupsByClusterId(getAffinityGroup().getClusterId())) {
+            List<Guid> list1 = ListUtils.subtract(vmIdList, ag.getEntityIds());
+            List<Guid> list2 = ListUtils.subtract(ag.getEntityIds(), vmIdList);
+            if (list1.isEmpty() && list2.isEmpty() && isEnforcing == ag.isEnforcing() && isPositive == ag.isPositive()) {
+                return failCanDoAction(EngineMessage.ACTION_TYPE_FAILED_AFFINITY_GROUP_CONTENT_REPETITION);
+            }
         }
         return validateParameters();
     }
